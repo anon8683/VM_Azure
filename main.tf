e75f7aeefcd39a669234957c2816d56fefc27e2e
unknown <flightbuzz1234@gmail.com>
2025-06-16 01:39:42 +0100

    New VM added, renamed vm-01, vm-02

--- main.tf
+++ main.tf
@@ -74,7 +74,7 @@ resource "azurerm_network_interface_security_group_association" "nic_nsg_assoc" 
 
 
 
-resource "azurerm_linux_virtual_machine" "vm" {
+resource "azurerm_linux_virtual_machine" "vm-01" {
   name                            = var.vm_name
   resource_group_name             = azurerm_resource_group.rg.name
   location                        = azurerm_resource_group.rg.location
@@ -99,3 +99,30 @@ resource "azurerm_linux_virtual_machine" "vm" {
   }
 
 }
+
+resource "azurerm_linux_virtual_machine" "vm-02" {
+  name                            = var.vm_name
+  resource_group_name             = azurerm_resource_group.rg.name
+  location                        = azurerm_resource_group.rg.location
+  size                            = "Standard_DS1_v2"
+  admin_username                  = var.admin_username
+  admin_password                  = var.admin_password
+  disable_password_authentication = false
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
+    version   = "latest"
+  }
+
+}
+
