64369435e47bf5aa2ddd92b5c0e2115b3f3510d5
unknown <flightbuzz1234@gmail.com>
2025-06-16 02:40:25 +0100

    Changed NIC to Dynamic from Static

--- main.tf
+++ main.tf
@@ -50,7 +50,7 @@ resource "azurerm_network_interface" "nic" {
     name                          = "internal"
     public_ip_address_id          = azurerm_public_ip.pip[count.index].id
     subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Static"
+    private_ip_address_allocation = "Dynamic"
   }
 }
 
